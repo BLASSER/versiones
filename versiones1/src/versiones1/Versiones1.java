b82bbeabbbb41795ad87b9e08ddf692bae5bffe8
BLASSER <blasserfull@gmail.com>
2017-06-07 19:51:49 -0600

    ejemplo1 realizado con exito

---
+++ versiones1/src/versiones1/Versiones1.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package versiones1;
+
+/**
+ *
+ * @author iron_
+ */
+public class Versiones1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        problemas i = new problemas();
+        i.setVisible(true);
+        i.setLocationRelativeTo(null);
+    }
+    
+}
